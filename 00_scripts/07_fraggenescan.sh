d8faf62e3bda3b3bfd671b4a67028ae8997e1c31
jleluyer <jeremy.le.luyer@gmail.com>
2018-09-12 16:05:24 -1000

    Update 07_fraggenescan.sh

--- 00_scripts/07_fraggenescan.sh
+++ 00_scripts/07_fraggenescan.sh
@@ -11,16 +11,14 @@ cd $PBS_O_WORKDIR
 
 #usage sorterna
 base=__BASE__
-REPO_predict="/scratch/home1/jleluyer/capamax/04_filtered_non_rna_predict"
-REPO_fastqjoin="/scratch/home1/jleluyer/capamax/04_filtered_non_rna_join"
-
+REPO_predict="04_filtered_non_rna_predict"
+REPO_fastqjoin="04_filtered_non_rna_join"
 
 FragGeneScan.pl -s "$REPO_fastqjoin"/"$base".un1 -o "$REPO_predict"/"$base".un1 -0 -t illumina_5 -p 10
 
 FragGeneScan.pl -s "$REPO_fastqjoin"/"$base".un2 -o "$REPO_predict"/"$base".un2 -0 -t illumina_5 -p 10
 
 FragGeneScan.pl -s "$REPO_fastqjoin"/"$base".join -o "$REPO_predict"/"$base".join -0 -t illumina_5 -p 10
-
 
 # combin by individuals
 cat "$REPO_predict"/"$base".join "$REPO_predict"/"$base".un2 "$REPO_predict"/"$base".un1 >"$REPO_predict"/"$base".combined
