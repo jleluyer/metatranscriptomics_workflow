04ce5d14cee4f87dc06633f06c87b1847ffaf7df
jleluyer <jeremy.le.luyer@gmail.com>
2018-09-12 16:06:12 -1000

    Update 07_fraggenescan_jobs.sh

--- 00_scripts/datarmor_jobs/07_fraggenescan_jobs.sh
+++ 00_scripts/datarmor_jobs/07_fraggenescan_jobs.sh
@@ -4,7 +4,7 @@
 rm 00_scripts/datarmor_jobs/PREDICT*sh
 
 # launch scripts for Colosse
-for file in $(ls /scratch/home1/jleluyer/capamax/04_filtered_non_rna_join/*|sed -e 's/.un[12]//g' -e 's/.join//g' |sort -u)
+for file in $(ls 04_filtered_non_rna_join/*|sed -e 's/.un[12]//g' -e 's/.join//g' |sort -u)
 
 do
 	base=$(basename "$file")
